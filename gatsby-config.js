ec80a9d99824ccd1a8a1d401a2cf6a12cd9db8fe
Vincent3054 <ok22758469>
2022-07-03 01:22:42 +0800

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,5 +34,15 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+      'gatsby-plugin-offline',
+      'gatsby-plugin-sass',
+      'gatsby-transformer-remark',
+      {
+        resolve: 'gatsby-source-filesystem',
+        options: {
+          path: `${__dirname}/src/pages`,
+          name: 'pages',
+        }
+      }
   ],
 }
