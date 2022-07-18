9ffa1c4e8abd27c316fd0297b2fcb4eb45fe3b00
Vincent3054 <ok96305@gmail.com>
2022-07-18 14:30:12 +0800

    Deploy setting 4

--- src/pages/index.js
+++ src/pages/index.js
@@ -84,12 +84,8 @@ const IndexPage = () => (
             />
             <h1>
                 Welcome to <b>Gatsby!</b>
-                <Link to="/page-3/">Go to NEW page</Link>
                 <Link to="/home/">Go to Home page</Link>
                 <Link to="/resume/">Go to resume page</Link>
-
-
-
             </h1>
             <p className={styles.intro}>
                 <b>Example pages:</b>{" "}
